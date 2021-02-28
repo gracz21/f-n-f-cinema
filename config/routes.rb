17d06122613c23d219723111079e5be29a4f34f1
gracz21 <kamil.walkowiak@netguru.com>
2021-02-28 17:46:19 +0100

    Configure devise routes
    Set custom names for login/logout paths, expect JSON payload format by default

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users,
+             defaults: { format: :json },
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout'
+             }
 end
