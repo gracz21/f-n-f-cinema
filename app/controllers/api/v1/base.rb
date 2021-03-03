d5ef44f3c4659f71bece6b5fd8090bded7a3f001
gracz21 <kamil.walkowiak@netguru.com>
2021-03-03 18:05:25 +0100

    Add base class for API V1

---
+++ app/controllers/api/v1/base.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module API
+  module V1
+    class Base < Grape::API
+      version 'v1', using: :path
+
+      content_type :json, 'application/vnd.api+json'
+      default_format :json
+
+      # Verify Pundit policy was called (or skip_authorization was called explicitly)
+      after { verify_authorized }
+
+      mount CinemaWorkers::Base
+      mount Users::Base
+    end
+  end
+end
